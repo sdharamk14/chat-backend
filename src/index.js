8459a391ddd0436ab9382c7636901d3f12f938da
sdharamk14 <sdharamkar14@gmail.com>
2022-01-04 15:31:03 +0000

    Adding changes

--- src/index.js
+++ src/index.js
@@ -4,11 +4,32 @@ const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-
-io.on("connection", (socket) => {
-  console.log("a user connected");
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
-server.listen(3000, () => {
-  console.log("listening on *:3000");
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
 });
+
+server.listen(4000, () => {
+  console.log("listening on *:4000");
+});
+
+
+// const io = new Server(server, {
+//   cors: {
+//     origin: 'http://localhost:5500',
+//   }
+// });
+// io.on("connection", (socket) => {
+//   console.log("a user connected");
+// });
+
+
